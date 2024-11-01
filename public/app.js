440a447a24d1372ea85cd86c9ea18cfd06eff877
Deface <info@xnail.shop>
2024-11-01 12:50:03 +0200

    first commit

--- public/app.js
+++ public/app.js
@@ -1,6 +1,4 @@
-// Ждем, когда вся страница будет загружена и готова к взаимодействию
 $(document).ready(function () {
-
     // Обновляем количество товаров в корзине при загрузке страницы
     updateCartCount();
 
@@ -85,57 +83,67 @@ $(document).ready(function () {
         displayCart();
     });
 
-   // Функция для отображения корзины
-   function displayCart() {
-    const cartItems = $('#cart-items');
-    cartItems.empty(); 
-    let totalPrice = 0;
+    // Функция для отображения корзины
+    function displayCart() {
+        const cartItems = $('#cart-items');
+        cartItems.empty(); 
+        let totalPrice = 0;
 
-    // Отображение элементов корзины
-    cart.forEach(item => {
-        totalPrice += item.price * item.quantity; // Учитываем количество при расчете стоимости
-        cartItems.append(`
-            <li class="list-group-item">
-                ${item.name} - ${item.price.toFixed(2)} x ${item.quantity} 
-                <button data-id="${item.id}" class="btn btn-outline-secondary change-quantity increment">+</button>
-                <button data-id="${item.id}" class="btn btn-outline-secondary change-quantity decrement">-</button>
-                <button data-id="${item.id}" class="btn btn-danger remove-item">Удалить</button>
-            </li>
-        `);
-    });
+        // Проверяем, есть ли товары в корзине
+        if (cart.length === 0) {
+            $('#cart-items').html(`<li class="list-group-item text-center">Корзина пуста!</li>`);
+            $("#place-order").hide();
+            $("#orderForm").hide();
+            $('#total-price').text('');
+        } else {
+            // Отображение элементов корзины
+            cart.forEach(item => {
+                totalPrice += item.price * item.quantity; // Учитываем количество при расчете стоимости
+                cartItems.append(`
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        ${item.name} - ${item.price.toFixed(2)} x ${item.quantity} 
+                        <div>
+                            <button data-id="${item.id}" class="btn btn-outline-secondary change-quantity increment">+</button>
+                            <button data-id="${item.id}" class="btn btn-outline-secondary change-quantity decrement">-</button>
+                            <button data-id="${item.id}" class="btn btn-danger remove-item">Удалить</button>
+                        </div>
+                    </li>
+                `);
+            });
 
-    $('#total-price').text(`Всего: ${totalPrice.toFixed(2)}`);
-    const cartModal = new bootstrap.Modal(document.getElementById('cart'), { keyboard: false });
-    cartModal.show();
-}
+            $('#total-price').text(`Всего: ${totalPrice.toFixed(2)}`);
+        }
+
+        const cartModal = new bootstrap.Modal(document.getElementById('cart'), { keyboard: false });
+        cartModal.show();
+    }
 
     function displayProducts(products) {
         $('#product-list').empty(); 
+
         products.forEach(product => {
             const discountBadge = product.discount ? `<span class="badge bg-success position-absolute top-0 end-0 m-2">Скидка: ${product.discount}%</span>` : '';
             const saleBadge = product.onSale ? `<span class="badge bg-danger position-absolute top-0 start-0 m-2">Распродажа</span>` : '';
-    
+
             const productHtml = `
-                <div class="col-md-4 position-relative">
-                    <div class="card mb-4 shadow-sm text-center">
-                        <img src="${product.image}" class="card-img-top" alt="${product.name}" style="object-fit: cover; height: 200px;">
+                <div class="col-12 col-md-6 col-lg-4 position-relative">
+                    <div class="card mb-4 shadow-sm text-center" style="border-radius: 15px;">
+                        <img src="${product.image}" class="card-img-top" alt="${product.name}" style="object-fit: cover; height: 200px; border-top-left-radius: 15px; border-top-right-radius: 15px;" data-id="${product.id}" class="product-image">
                         ${discountBadge} ${saleBadge}
                         <div class="card-body">
                             <h5 class="card-title">${product.name}</h5>
                             <h6 class="brand-name">Бренд: ${product.brand}</h6>
-                            <p>Артикул: ${product.sku}</p>
-                            <p>Серийный номер: ${product.serial}</p>
+                            <p style="display: flex; justify-content: space-between;">
+                                <span>Артикул: ${product.sku}</span>
+                                <span>Серийный номер: ${product.serial}</span>
+                            </p>
                             <h3 class="card-price">${product.price.toFixed(2)}</h3>
                             <div class="card-text">
                                 ${renderStars(product.rating)}
                                 <span> (${product.votes})</span>
                             </div>
-                            <div class="input-group mb-3">
-                                <button class="btn btn-outline-secondary change-quantity decrement">−</button>
-                                <input type="number" class="form-control product-quantity" value="1" min="1" style="width: 60px;">
-                                <button class="btn btn-outline-secondary change-quantity increment">+</button>
-                            </div>
-                            <button data-id="${product.id}" data-name="${product.name}" data-price="${product.price}" class="btn btn-primary add-to-cart">Добавить в корзину</button>
+                            <button data-id="${product.id}" data-name="${product.name}" data-price="${product.price}" class="btn btn-primary add-to-cart">Buy</button>
+                            <button data-id="${product.id}" class="btn btn-secondary show-attributes">Spec</button>
                         </div>
                     </div>
                 </div>
@@ -143,28 +151,47 @@ $(document).ready(function () {
             $('#product-list').append(productHtml);
         });
 
+        // Обработчик событий для увеличения изображения товара
+        $('.product-image').on('click', function () {
+            const imgSrc = $(this).attr('src');
+            $('#image-modal .modal-body img').attr('src', imgSrc);
+            const imageModal = new bootstrap.Modal($('#image-modal'));
+            imageModal.show();
+        });
+
+        // Обработчик событий для кнопки "Характеристики"
+        $('.show-attributes').on('click', function () {
+            const productId = $(this).data('id');
+            showProductAttributes(productId);
+        });
 
         // Обработчик событий для добавления товара в корзину
         $('.add-to-cart').on('click', function () {
-            const productId = $(this).data('id');
-            const productName = $(this).data('name');
-            const productPrice = parseFloat($(this).data('price'));
+            const $button = $(this);
+            const productId = $button.data('id');
+            const productName = $button.data('name');
+            const productPrice = parseFloat($button.data('price'));
+            const quantity = 1; // фиксируем количество на 1, так как это начальная добавка в корзину
 
             const product = {
                 id: productId,
                 name: productName,
-                price: productPrice
+                price: productPrice,
+                quantity: quantity // Добавляем количество
             };
 
             // Проверяем, есть ли продукт уже в корзине
             const existingProduct = cart.find(item => item.id === productId);
             if (!existingProduct) {
                 cart.push(product);
-                localStorage.setItem('cart', JSON.stringify(cart));
+                localStorage.setItem('cart', JSON.stringify(cart)); // Сохраняем в localStorage
                 console.log('Товар добавлен в корзину!');
                 updateCartCount();
+                $button.text("Товар добавлен").css("background-color", "black").prop("disabled", true); // Меняем текст и цвет кнопки
             } else {
-                console.log('Этот товар уже в корзине.');
+                existingProduct.quantity += quantity; // Увеличиваем количество товара в корзине
+                localStorage.setItem('cart', JSON.stringify(cart)); // Обновляем localStorage
+                console.log('Количество товара обновлено в корзине');
             }
         });
 
@@ -192,11 +219,42 @@ $(document).ready(function () {
         });
     }
 
+    // Функция для отображения характеристик товара
+    function showProductAttributes(productId) {
+        $.get(`${PRODUCT_URL}/${productId}`)
+            .done(function (data) {
+                console.log("TEST");
+                const product = JSON.parse(data);
+                let attributesHtml = '<table class="table table-striped">';
+                attributesHtml += `<thead>
+                                     <tr>
+                                         <th>Характеристика</th>
+                                         <th>Значение</th>
+                                     </tr>
+                                   </thead><tbody>`;
+                for (const [key, value] of Object.entries(product.attributes)) {
+                    attributesHtml += `<tr>
+                                         <td>${key}</td>
+                                         <td>${value}</td>
+                                       </tr>`;
+                }
+                attributesHtml += '</tbody></table>';
+
+                $('#attributes-modal .modal-body').html(attributesHtml);
+                const attributesModal = new bootstrap.Modal($('#attributes-modal'));
+                attributesModal.show();
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                console.error("Ошибка загрузки характеристик продукта: ", textStatus, errorThrown);
+                alert("Не удалось загрузить характеристики. Пожалуйста, попробуйте позже.");
+            });
+    }
+
     // Функция для отрисовки звездочек на основе рейтинга
     function renderStars(rating) {
         let starsHtml = '';
         for (let i = 1; i <= 5; i++) {
-            starsHtml += `<span class="star ${i <= rating ? 'checked' : ''}" data-value="${i}" style="font-size: 24px; color: gold;">&#9733;</span>`;
+            starsHtml += `<span class="star ${i <= rating ? 'checked' : ''}" data-value="${i}" style="font-size: 36px; color: gold;">&#9733;</span>`;
         }
         return starsHtml;
     }
@@ -236,15 +294,13 @@ $(document).ready(function () {
     // Функция для проверки телефонного номера
     function validatePhone(phone) {
         // Регулярное выражение для проверки формата телефона
-        const phonePattern = /^\+?[0-9]{7,15}$/; // Примерный формат телефонов
+        const phonePattern = /^\+?[0-9]{7,15}$/; 
         return phonePattern.test(phone);
     }
 
     // Функция для проверки адреса электронной почты
     function validateEmail(email) {
-        // Регулярное выражение для проверки формата электронной почты
-
-        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Стандартное выражение для почты
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
         return emailPattern.test(email);
     }
 });
